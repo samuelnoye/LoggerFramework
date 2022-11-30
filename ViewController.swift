91e492fc5f973a38675ecd457e91fa149993114b
Noye Samuel <samuel.noye@amalitech.org>
2022-11-30 16:42:59 +0000

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoggerFramework
+//
+//  Created by Noye Samuel on 30/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
